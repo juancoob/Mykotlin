1f53c5577d3b06fa44aa9ed02736d66ee04a6791
juancoob <ja.cobos.obrero@gmail.com>
2018-10-24 13:56:04 +0200

    feat: Updated MainActivity to start the main recyclerView setup

--- app/src/main/java/com/juancoob/nanodegree/and/mykotlin/MainActivity.kt
+++ app/src/main/java/com/juancoob/nanodegree/and/mykotlin/MainActivity.kt
@@ -2,6 +2,8 @@ package com.juancoob.nanodegree.and.mykotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,9 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkFirstLetter();
-        setMainMessage();
-        showToast(getString(R.string.hello_world));
+        checkFirstLetter()
+        setMainMessage()
+        showToast(getString(R.string.hello_world))
+        //populateWeatherList()
+        initRecyclerView()
     }
 
     /**
@@ -53,6 +57,20 @@ class MainActivity : AppCompatActivity() {
      *  Set the main message
      */
     fun setMainMessage() {
-        tv_hello_message.text = getString(R.string.hello_kotlin);
+        tv_hello_message.text = getString(R.string.hello_kotlin)
+    }
+
+    /**
+     * Init the dummy list to show it on the recyclerView
+     */
+    private fun populateWeatherList() = listOf(getString(R.string.monday), getString(R.string.tuesday), getString(R.string.wednesday),
+                getString(R.string.thursday), getString(R.string.friday), getString(R.string.saturday), getString(R.string.sunday))
+
+    /**
+     * Initialise the recyclerView to show the weather
+     */
+    private fun initRecyclerView() {
+        val forecastList = findViewById<RecyclerView>(R.id.rv_forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
     }
 }
