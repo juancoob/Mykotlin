c8f0648283e3bfafea238c62269d5d7effc46f51
juancoob <ja.cobos.obrero@gmail.com>
2018-10-31 21:32:56 +0100

    feat: Updated ResponseClasses adding new data classes

--- app/src/main/java/com/juancoob/nanodegree/and/mykotlin/data/ResponseClasses.kt
+++ app/src/main/java/com/juancoob/nanodegree/and/mykotlin/data/ResponseClasses.kt
@@ -1,5 +1,7 @@
 package com.juancoob.nanodegree.and.mykotlin.data
 
+import java.util.*
+
 /**
  * Created by Juan Antonio Cobos Obrero on 28/10/18.
  */
@@ -10,11 +12,13 @@ data class City(val id: Long, val name : String, val coord : Coordinates, val co
 
 data class Coordinates(val lon : Float, val lat : Float)
 
-/*data class Forecast(val dt : Long, val temp : Temperature, val pressure : Float, val humidity : Int,
+data class Forecast(val dt : Long, val temp : Temperature, val pressure : Float, val humidity : Int,
                     val weather : List<Weather>, val speed : Float, val deg : Int, val clouds : Int,
-                    val rain : Float)*/
+                    val rain : Float)
 
 data class Temperature(val day: Float, val min: Float, val max: Float, val night: Float,
                        val eve: Float, val morn: Float)
 
 data class Weather(val id: Long, val main: String, val description: String, val icon: String)
+
+data class dummyForecast(val date : Date, val temperature : Float, val detail : String)
